9560e7b2365d9b05b89531108730819a09f45962
Wesley Kaihara <wesleykaihara@gmail.com>
2026-03-01 21:11:07 -0300

    Remove unused imports

--- internal/adapters/driven/storage/s3.adapter.go
+++ internal/adapters/driven/storage/s3.adapter.go
@@ -8,7 +8,6 @@ import (
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
-	"github.com/aws/aws-sdk-go-v2/credentials"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 
 	"video_processor_service/internal/common/config/env"
--- internal/common/infra/queue/sqs-publisher.adapter.go
+++ internal/common/infra/queue/sqs-publisher.adapter.go
@@ -7,7 +7,6 @@ import (
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
-	"github.com/aws/aws-sdk-go-v2/credentials"
 	"github.com/aws/aws-sdk-go-v2/service/sqs"
 )
 
